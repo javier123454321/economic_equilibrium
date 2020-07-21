26105fae5b78a136b27bcaba68c5d68544828475
Javier Gonzalez <javier.e.glz@gmail.com>
2020-07-21 18:19:02 -0500

    feat: refactor code

--- src/widgets/renderSumWidget.js
+++ src/widgets/renderSumWidget.js
@@ -3,12 +3,29 @@
     let Qdemand = Mdemand * price + Bdemand
     return Qsupply == Qdemand;
  }
- 
+
+ function calculateSupplyVsDemand(supply, price, consumption, Msupply, Mdemand, Bsupply, Bdemand) {
+    if (consumption > supply) {
+        consumption = supply;
+        return "Demand is greater than supply, the company would have to either increase price or produce more.";
+    }
+
+    if (consumption <= 0) {
+        consumption = 0;
+        return "This price is higher than what people are willing to pay";
+    }
+
+    if (isMaxRevenue(Msupply, Mdemand, Bsupply, Bdemand, price)) {
+        return "This is the <a href='https://en.wikipedia.org/wiki/Economic_equilibrium'>equilibrium price</a>";
+    }
+}
+
  export default function renderSumWidget(){
     let Mdemand = -500;
     let Bdemand = 10000;
     let Msupply = 0; // what if ABC can hire more people when price goes up?
     let Bsupply = 8000;
+
     let consumption;
     let supply;
     let message;
@@ -27,23 +44,18 @@
                 break;
             }
         }
-        
-        consumption = price * Mdemand + Bdemand;
-        supply = price * Msupply + Bsupply;
+        consumption = (price * Mdemand) + Bdemand;
+        supply = (price * Msupply )+ Bsupply;
     
-        if (consumption > supply) {
-            consumption = supply;
-            message = "Demand is greater than supply, the company would have to either increase price or produce more.";
-        }
-    
-        if (consumption <= 0) {
-            consumption = 0;
-            message = "This price is higher than what people are willing to pay";
-        }
-
-        if (isMaxRevenue(Msupply, Mdemand, Bsupply, Bdemand, price)) {
-            message = "This is the <a href='https://en.wikipedia.org/wiki/Economic_equilibrium'>equilibrium price</a>"
-        }
+        message = calculateSupplyVsDemand(
+                supply, 
+                price, 
+                consumption, 
+                Msupply, 
+                Mdemand, 
+                Bsupply, 
+                Bdemand
+            );
     
         let revenue = consumption * price;
 

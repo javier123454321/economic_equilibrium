cc343da93f8a7fb923f4dba5012e0fdb6830a5f5
Javier Gonzalez <javier.e.glz@gmail.com>
2020-07-22 00:30:12 -0500

    feat: add a fetch for the wikipedia extract

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import renderProductWidget from './widgets/renderProductWidget.js';
 import renderSumWidget from './widgets/renderSumWidget.js';
-import renderSupplyAndDemandEquations from './widgets/renderSupplyAndDemandWidget.js'
+import renderSupplyAndDemandEquations from './widgets/renderSupplyAndDemandWidget.js';
+import setupToolTip from './widgets/renderWikipediaInfo.js';
 
 export default function renderApp(){
     renderProductWidget();
     renderSumWidget();
     renderSupplyAndDemandEquations();
+    setupToolTip();
 };
 renderApp();
---
+++ src/widgets/renderWikipediaInfo.js
@@ -0,0 +1,15 @@
+
+export default function setupTooltip(){
+    let tooltipIsRendered = false
+    document.getElementById("wikipedia-info").addEventListener('mouseover', () => {
+        if(!tooltipIsRendered){
+            fetch("https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=equilibrium%20price")
+            .then(response => response.text())
+            .then(result => {
+                console.log(JSON.parse(result).query.pages["227572"].extract);
+                document.getElementById('wikipedia-extract').innerText += JSON.parse(result).query.pages["227572"].extract;
+                tooltipIsRendered = true;
+            })
+        }
+    })
+}

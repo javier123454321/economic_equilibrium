ff96cab742d5b0e80f44994e704dd9b117dc3516
Javier Gonzalez <javier.e.glz@gmail.com>
2020-07-21 23:46:53 -0500

    feat: add reactive equations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import renderProductWidget from './widgets/renderProductWidget.js';
 import renderSumWidget from './widgets/renderSumWidget.js';
+import renderSupplyAndDemandEquations from './widgets/renderSupplyAndDemandWidget.js'
 
 export default function renderApp(){
     renderProductWidget();
     renderSumWidget();
+    renderSupplyAndDemandEquations();
 };
-renderApp()
+renderApp();

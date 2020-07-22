c3ce2770f2c95ee9747bf66283ca503eb580d212
Javier Gonzalez <javier.e.glz@gmail.com>
2020-07-22 12:54:48 -0500

    fix: update dependencies

--- tests/renderSupplyAndDemandEquations.spec.js
+++ tests/renderSupplyAndDemandEquations.spec.js
@@ -1,4 +1,4 @@
-import renderSupplyAndDemandEquations from '../public/src/widgets/renderSupplyAndDemandWidget.js'
+import renderSupplyAndDemandEquations from '../public/src/widgets/RenderSupplyAndDemandWidget.js'
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');

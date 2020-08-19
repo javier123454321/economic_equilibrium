12d48007194f0c946906556f693f7d2930397027
Javier Gonzalez <javier.e.glz@gmail.com>
2020-08-19 00:30:40 -0500

    fix: issue with rendering the equilibrium

--- public/src/widgets/renderSumWidget.js
+++ public/src/widgets/renderSumWidget.js
@@ -1,6 +1,16 @@
  import { toCurrency } from '../services/CurrencyService.js';
 
  function calculateSupplyVsDemand(supply, consumption, price, revenue) {
+
+    if ( Math.abs(supply - consumption) <= 1 ) {
+        return `
+        <h2>$${price} is the equilibrium price</h2> 
+        <h4>Revenue: $${ toCurrency(revenue, false) } per month</h4>
+        <h4>Products sold at this price: ${Math.floor(consumption)} units</h4>
+        <h4>Change the price to see how the market behaves with fluctuations</h4>
+        `;
+    }
+
     if (consumption > supply) {
         return `
         <h3>Price is too low at $${price}</h3>
@@ -16,20 +26,11 @@
         <h3>Price is Too High at $${price}</h3>
         No one will buy at this price`;
     }
-
-    if (Math.floor(supply - consumption) == 0 ) {
-        return `
-        <h2>$${price} is the equilibrium price</h2> 
-        <h4>Revenue: $${ toCurrency(revenue, false) } per month</h4>
-        <h4>Products sold at this price: ${Math.floor(consumption)} units</h4>
-        <h4>Change the price to see how the market behaves with fluctuations</h4>
-        `;
-    }
     return `
     <h3>$${price} is too expensive</h3> 
     <h4>Revenue: $${ toCurrency(revenue, false) } per month</h4>
     <h4>Products sold at this price: ${Math.floor(consumption)} units</h4>
-    People will buy some, but there will be <span class='emphasis'>${Math.floor( supply - consumption )}</span> items left over as it is too expensive`
+    People will buy some, but there will be <span class='emphasis'>${ Math.floor( supply - consumption ) }</span> items left over as it is too expensive`
 }
 
  export default function renderSumWidget(){

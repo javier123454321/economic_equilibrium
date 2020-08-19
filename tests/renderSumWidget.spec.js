dd6a31c37ab15286cd0f54320b02243923626ea0
Javier Gonzalez <javier.e.glz@gmail.com>
2020-08-19 01:15:39 -0500

    fix: failing unit tests

--- tests/renderSumWidget.spec.js
+++ tests/renderSumWidget.spec.js
@@ -1,4 +1,6 @@
-import renderSumWidget from '../public/src/widgets/RenderSumWidget.js'
+import renderSumWidget from '../public/src/widgets/RenderSumWidget.js';
+import { updatePrice } from '../public/src/widgets/RenderPriceWidget.js';
+
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');
@@ -15,33 +17,21 @@ function renderInput(price){
 describe('renderSumWidget', function() {
     beforeEach(() => {
         document.documentElement.innerHTML = html.toString();
-        document.getElementById('xyz-widget').innerHTML = renderInput("6.00");
         renderSumWidget();
     })
+
     it('Should display an the economic equilibrium when loading the page', () => {
-        expect( document.getElementById('result').textContent ).toBe(`
-        Amount of Products Sold: 
-        70 per month at $6 
-         Yields: 
-        Revenue: $420.00 per month
-        This is the equilibrium price`);
-        expect( document.getElementById('result').querySelector('p') ).toBeFalsy();
+        expect( document.getElementById('result').querySelector('h2').textContent ).toBe(`$6.00 is the equilibrium price`);
         }
     );
-    it('Should render an output when a user selects an input', () => {
-        document.getElementById("calculate").click();
-        expect( document.getElementById('result').querySelector('h3').textContent ).toBe("Amount of Products Sold:");
-    })
-    it('Should render the success message when equilibrium is reached, and descriptive errors when not', () => {
-        document.getElementById('xyz-widget').innerHTML = renderInput("30");
-        expect( document.getElementById('result').querySelectorAll('p.output-message')[0].textContent ).toBe("This price is higher than what people are willing to pay, there is excess supply");
-        
-        document.getElementById('xyz-widget').innerHTML = renderInput("3");
-        document.getElementById("calculate").click();
-        expect( document.getElementById('result').querySelectorAll('p.output-message')[0].textContent.substr(0, 30) ).toBe("Demand is greater than supply,");
 
-        document.getElementById('xyz-widget').innerHTML = renderInput("4.50");
-        document.getElementById("calculate").click();
-        expect( document.getElementById('result').querySelectorAll('p.output-message')[0].textContent.substr(0, 39) ).toBe("People will buy some, but there will be");
+    it('Should update when the price is updated', () => {
+        updatePrice(0.01);
+        expect( document.getElementById('result').querySelector('h3').textContent ).toBe("Price is too low at $0.01");
+    })
+
+    it('Should render the success message when equilibrium is reached, and descriptive errors when not', () => {
+        updatePrice(8);
+        expect( document.getElementById('result').querySelector('h3').textContent ).toBe("$8.00 is too expensive");
     })
 })
